3d9db8a1cc721fd29534e58d7a14352c1ef66dd6
NeulboGom <jinhee930409@inha.edu>
2023-02-03 14:52:47 +0900

    DS Example: Insert Function

--- day15.py
+++ day15.py
@@ -1,15 +1,22 @@
-pokemons = []  # 빈 배열
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
 
-def add_data(pokemon):
-    pokemons.append(None)
-    # pokemons[len(pokemons-1)] = pokemon
-    pokemons[-1] = pokemon
+    pokemons.append(None)  # 빈칸 추가
+    # kLen = len(pokemons)  # 배열의 현재 크기
+
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬북이')
-add_data('버터플')
-
-print(pokemons)
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "꼬부기", "버터플", "야도란"]
+    print(pokemons)
+    insert_data(2, '파이리')
+    print(pokemons)
+    insert_data(6, "피죤투")
+    print(pokemons)
