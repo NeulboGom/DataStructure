d85b7d4f9915ec419eb273efb2d7562038931cfc
NeulboGom <jinhee930409@inha.edu>
2023-02-03 14:51:28 +0900

    DS Example 99p

---
+++ day15.py
@@ -0,0 +1,15 @@
+pokemons = []  # 빈 배열
+
+def add_data(pokemon):
+    pokemons.append(None)
+    # pokemons[len(pokemons-1)] = pokemon
+    pokemons[-1] = pokemon
+
+
+add_data('피카츄')
+add_data('라이츄')
+add_data('파이리')
+add_data('꼬북이')
+add_data('버터플')
+
+print(pokemons)
