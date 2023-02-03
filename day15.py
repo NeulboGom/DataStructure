82586b51597b392d74d52d6438ce90d060981b42
NeulboGom <jinhee930409@inha.edu>
2023-02-03 14:53:29 +0900

    DS Example: Delete Function

--- day15.py
+++ day15.py
@@ -1,22 +1,22 @@
-def insert_data(idx, pokemon):
+pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '버터플']
+
+
+def delete_data(idx):
     if idx < 0 or idx > len(pokemons):
-        print("Out of range!")
+        print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
-    pokemons.append(None)  # 빈칸 추가
-    # kLen = len(pokemons)  # 배열의 현재 크기
+    len_of_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
 
-    for i in range(len(pokemons) - 1, idx, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
+    for i in range(idx + 1, len_of_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
 
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+    del (pokemons[len_of_pokemons - 1])
 
 
-if __name__ == "__main__":
-    pokemons = ["피카츄", "라이츄", "꼬부기", "버터플", "야도란"]
-    print(pokemons)
-    insert_data(2, '파이리')
-    print(pokemons)
-    insert_data(6, "피죤투")
-    print(pokemons)
+delete_data(1)
+print(pokemons)
+delete_data(3)
+print(pokemons)
