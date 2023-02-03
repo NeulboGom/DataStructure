ed2e95113e77c4d55a0f0d5936cb7bed4474af0a
NeulboGom <jinhee930409@inha.edu>
2023-02-03 14:54:49 +0900

    DS Example: Delete Function 2

--- day15.py
+++ day15.py
@@ -1,22 +1,34 @@
 pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '버터플']
+print(pokemons)
+
+print("=" * 50)
 
 
 def delete_data(idx):
+    '''
+    선형 리스트의 idx 번째 뒤로 다 삭제
+    :param idx: idx
+    :return: [:idx]
+    '''
     if idx < 0 or idx > len(pokemons):
         print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
-    len_of_pokemons = len(pokemons)
-    pokemons[idx] = None  # 데이터 삭제
+    # len_of_pokemons = len(pokemons)
 
-    for i in range(idx + 1, len_of_pokemons):
-        pokemons[i - 1] = pokemons[i]
-        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+    # for i in range(idx - 1, len_of_pokemons):
+    #     pokemons[i] = None
 
-    del (pokemons[len_of_pokemons - 1])
+    pokemons_list = pokemons[0:idx]
+    print(pokemons_list)
 
 
 delete_data(1)
-print(pokemons)
+print("=" * 50)
 delete_data(3)
-print(pokemons)
+
+# if __name__ == "__main__":
+#     pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '버터플']
+#     print(pokemons)
+#     delete_data(1)
+#     delete_data(3)
