eb7adfd328eebe44e8a57b60d6757cfc0d2089c6
NeulboGom <jinhee930409@inha.edu>
2023-02-03 14:55:30 +0900

    DS Example: Insert Function 2

--- day15.py
+++ day15.py
@@ -1,34 +1,28 @@
-pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '버터플']
-print(pokemons)
-
-print("=" * 50)
-
-
-def delete_data(idx):
+def insert_data(idx, pokemon):
     '''
-    선형 리스트의 idx 번째 뒤로 다 삭제
-    :param idx: idx
-    :return: [:idx]
+    선형 리스트 idx 위치의 원소 삽입
+    :param idx: 순번
+    :param pokemon: 포켓몬 종류
+    :return: final list
     '''
     if idx < 0 or idx > len(pokemons):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
+        print("Out of range!")
         return
 
-    # len_of_pokemons = len(pokemons)
+    pokemons.append(None)  # 빈칸 추가
+    # kLen = len(pokemons)  # 배열의 현재 크기
 
-    # for i in range(idx - 1, len_of_pokemons):
-    #     pokemons[i] = None
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
-    pokemons_list = pokemons[0:idx]
-    print(pokemons_list)
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
 
-delete_data(1)
-print("=" * 50)
-delete_data(3)
-
-# if __name__ == "__main__":
-#     pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '버터플']
-#     print(pokemons)
-#     delete_data(1)
-#     delete_data(3)
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "꼬부기", "버터플", "야도란"]
+    print(pokemons)
+    insert_data(2, '파이리')
+    print(pokemons)
+    insert_data(6, "피죤투")
+    print(pokemons)
