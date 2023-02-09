fac527045dbc5190fbaaefd17dc9523db8e9f723
NeulboGom <jinhee930409@inha.edu>
2023-02-09 15:49:53 +0900

    Chapter11 Advanced Sort: Quick Sort not working

--- day19.py
+++ day19.py
@@ -1,106 +1,145 @@
-# Chapter11 정렬: Sort
-
+# Chapter12 Sort Advanced
 import random
 
+# Quick Sort 퀵 정렬 p433
+# 최선: nlog(n) (variation is n) / 평균: nlog(n) / 최악: n**2 / 메모리: 뭐라는겨
+# 안정: 일반적인 제자리 정렬은 안정적이지 못하다. 안정판이 존재한다. / 방식: 파티셔닝
 
-# 구글에 파이썬 정렬 알고리즘 검색
-# Insertion Sort: 삽입 정렬 405p
-# 최선: n / 평균: n**2 / 최악: n**2 / 메모리: 1 / 안정: 예 / 방식: 삽입
-# 삽입 정렬도 선택정렬과 마찬가지로 연산 수는 O(n**2)
-# 입력 개수가 커질수록 기하급수적으로 비교 횟수(또는 연산 횟수)가 늘어나기에 성능이 좋지 않은 알고리즘
+# Merge Sort: 파티션을 나누고 합치는 것을 반복 - Divide and Conquer - 가르고 정복(?)한다...
 
-# 기존 데이터 중에서 자신의 위치를 찾아 데이터를 삽입하는 정렬
-# 삽입 정렬을 구하기 위해 함수를 정의해야 한다.
+# 퀵 정렬:
+# 먼저 기준(pivot)을 하나 뽑은 후, 기준보다 작은 그룹과 큰 그룹을 나누어 다시 각 그룹으로 정렬하는 방법
+# 이 떄 나눈 그룹을 다시 정렬하고자 재귀 호출을 하고, 각 그룹의 정렬이 완료되면 합치는 방식을 사용한다.
+# 기준(pivot): 어떤 것을 선정해도 상관은 없지만, 일반적으로 중간에 위치한 데이터를 선정한다.
 
-# insert할 위치 찾는 함수
-def insert_idx(ls, data):
-    idx = -1  # 초기값은 없는 위치로
-    for i in range(len(ls)):
-        if ls[i] > data:
-            idx = i
-            break
-    if idx == -1:  # 큰 값을 못 찾음 == 제일 마지막 위치
-        return len(ls)
-    else:
-        return idx
+########## 퀵 정렬의 간단 구현 #############
+## 전역 변수 선언 부분 ##
+count = 0
 
 
-test_list = []
-ins_idx = insert_idx(test_list, 55)
-print(f"삽입할 위치 -> {ins_idx}")
+######### 퀵 정렬 오름차순 ##########
+## 클래스와 함수 선언 부분 ##
+def quick_sort(ls):
+    global count
+    count += 1
+    n = len(ls)  # 리스트의 길이는 n
+    if n <= 1:  # 정렬할 리스트가 하나면 정렬할 필요 없음
+        return ls
 
-test_list = [44, 77, 88]
-ins_idx = insert_idx(test_list, 55)
-print(f"삽입할 위치 -> {ins_idx}")
+    pivot = ls[n // 2]  # 리스트의 중간에 위치한 값을 pivot으로 설정 / 나중에 들어올 리스트들의 pivot
+    left_ls, right_ls = [], []  # 좌측, 우측 요소를 저장할 빈 리스트 생성
 
-test_list = [44, 55, 77, 88]
-ins_idx = insert_idx(test_list, 100)
-print(f"삽입할 위치 -> {ins_idx}")
+    for number in ls:
+        if number < pivot:  # pivot보다 작으면 좌측에 저장
+            left_ls.append(number)
+        elif number > pivot:  # pivot보다 크면 우측에 저장
+            right_ls.append(number)
 
-# 삽입 정렬 구현
-# list의 insert 함수를 사용하여 입력 가능
-
-## 전역 변수 선언 구간 ##
-before = [188, 162, 168, 120, 50, 150, 177, 105]
-after = []
-
-## 메인 코드 부분 ##
-print(f"정렬 전 ==> {before}")
-for i in range(len(before)):
-    data = before[i]  # before의 요소들을 data에 하나씩 넣음
-    ins_idx = insert_idx(after, data)  # 에프터에 data 하나가 들어갈 위치 확인 / 처음엔 0일걸
-    after.insert(ins_idx, data)  # insert로 확인된 자리로 삽입
-
-print(f"정렬 후 ==> {after}")
-
-print('=' * 70)
+    return quick_sort(left_ls) + [pivot] + quick_sort(right_ls)  # 재귀함수로 요소 하나만 남을 때까지 계속 나누고
+    # 결국 정렬해서 중간에 pivot을 두고 다시 합침
 
 
-# p411 Self Study 11-2
+# 결국 pivot 값이 잘 잡히면 빠르게 끝나고, 아니면 오래 걸림
 
-## 함수선언 부분 ##
-def insert_idx_rev(ls, data):
-    idx = -1  # 초기값은 없는 위치로
-    for i in range(len(ls)):
-        if ls[i] < data:
-            idx = i
-            break
-    if idx == -1:  # 큰 값을 못 찾음 == 제일 마지막 위치
-        return len(ls)
-    else:
-        return idx
+check = [random.randint(0, 200) for _ in range(20)]
+print(f"정렬 전 --> {check}")
+
+print(f"정렬 후 --> {quick_sort(check)}")
+print(f"##{count}회로 정렬 완료")
+print("=" * 70)
 
 
-## 전역 변수 선언 구간 ##
-old = [random.randint(0, 200) for _ in range(10)]
-new = []
-
-## 메인 코드 부분 ##
-print(f"옛 리스트 -> {old}")
-for i in range(len(old)):
-    data = old[i]
-    ins_idx = insert_idx_rev(new, data)
-    new.insert(ins_idx, data)
-
-print(f"새 리스트 -> {new}")
-print('=' * 70)
-
-
-# 여태 방식은 리스트를 두개를 써서 공간이 많이 소요가 돼서
-# 리스트 하나로 데이터 정렬하는 쪽으로 효율적이게 바꾸자
+# Self Study 12-2 p437
+########## 퀵정렬 내림차순 ##########
 
 ## 클래스와 함수 선언 부분 ##
-def insertion_sort(ls):
-    for end in range(len(ls)):      # for문이 중첩돼서 n**2의 시간을 갖는다.
-        for current in range(end, 0, -1):
-            if ls[current - 1] > ls[current]:   # 앞의 것과 비교해서, 앞에 것보다 current가 작으면
-                ls[current], ls[current - 1] = ls[current - 1], ls[current] # current가 앞으로 가고, 앞에가 current로 온다.
-    return ls
+def quick_sort_rev(ls):
+    global count
+    count += 1
+    n = len(ls)  # 리스트의 길이는 n
+    if n <= 1:  # 정렬할 리스트가 하나면 정렬할 필요 없음
+        return ls
+
+    pivot = ls[n // 2]  # 리스트의 중간에 위치한 값을 pivot으로 설정 / 나중에 들어올 리스트들의 pivot
+    left_ls, right_ls = [], []  # 좌측, 우측 요소를 저장할 빈 리스트 생성
+
+    for number in ls:
+        if number > pivot:  # pivot보다 작으면 좌측에 저장
+            left_ls.append(number)
+        elif number < pivot:  # pivot보다 크면 우측에 저장
+            right_ls.append(number)
+
+    return quick_sort_rev(left_ls) + [pivot] + quick_sort_rev(right_ls)  # 재귀함수로 요소 하나만 남을 때까지 계속 나누고
+    # 결국 정렬해서 중간에 pivot을 두고 다시 합침
 
 
-## 전역 변수 선언 부분 ##
-test = [random.randint(0, 100) for _ in range(8)]
-print(f"정렬 전 => {test}")
+check2 = [random.randint(0, 200) for _ in range(20)]
+print(f"정렬 전 --> {check2}")
 
-print(f"정렬 후 => {insertion_sort(test)}")
+print(f"정렬 후 --> {quick_sort_rev(check2)}")
+print(f"##{count}회로 정렬 완료")
+print("=" * 70)
+
+
+############# 중복 값을 고려한 퀵 정렬 ###########
+
+## 클래스와 함수 선언 부분 ##
+def quick_sort_wmid(ls):
+    global count
+    count += 1
+    n = len(ls)  # 리스트의 길이는 n
+    if n <= 1:  # 정렬할 리스트가 하나면 정렬할 필요 없음
+        return ls
+
+    pivot = ls[n // 2]  # 리스트의 중간에 위치한 값을 pivot으로 설정 / 나중에 들어올 리스트들의 pivot
+    left_ls, right_ls, middle_ls = [], [], []  # 좌측, 우측, 중복 요소를 저장할 빈 리스트 생성
+
+    for number in ls:
+        if number < pivot:  # pivot보다 작으면 좌측에 저장
+            left_ls.append(number)
+        elif number > pivot:  # pivot보다 크면 우측에 저장
+            right_ls.append(number)
+        else:
+            middle_ls.append(number)
+
+    return quick_sort_wmid(left_ls) + [pivot] + quick_sort_wmid(right_ls)  # 재귀함수로 요소 하나만 남을 때까지 계속 나누고
+    # 결국 정렬해서 중간에 중복값 리스트를 두고 다시 합침
+
+
+# 퀵 정렬 일반 구현 / 리스트 하나로
+def quick_sort_advaced(ls, start, end):
+    global count
+    count += 1
+    if end <= start:
+        return
+
+    low = start
+    high = end
+
+    pivot = ls[(low + high) // 2]
+    while low <= high:
+        while ls[low] < pivot:
+            low += 1
+        while ls[high] > pivot:
+            high -= 1
+        if low <= high:
+            ls[low], ls[high] = ls[high], ls[low]
+            low, high = low + 1, high - 1
+
+    mid = low
+
+    quick_sort_advaced(ls, start, mid - 1)
+    quick_sort_advaced(ls, end, mid)
+
+
+def quick_sort_advanced_out(ls):
+    quick_sort_advaced(ls, 0, len(ls) - 1)
+
+
+check3 = [random.randint(0, 200) for _ in range(8)]
+print(f"정렬 전 --> {check3}")
+
+quick_sort_advanced_out(check3)
+print(f"정렬 후 --> {check3}")
+print(f"## {count}회로 정렬 완료")
 print("="*70)
